dc63b56d8429b38783acaee6a383e0bcc057f579
Patelkhana Sai prasant <patelkhanasaiprasant@gmail.com>
2022-08-08 07:27:01 -0400

    Update setup_RAN_env_USM1_USM.sh

--- setup_RAN_env_USM1_USM.sh
+++ setup_RAN_env_USM1_USM.sh
@@ -1,11 +1,7 @@
 # Load file
 #!/usr/bin/env bash
  
-export hostname=usm1
+export hostname=nfv127
 export api_payload=input-source
-export api_port=9091
-export api_url_create_ne='/tip/v1/CreateNE/oss/ne/'
-export api_url_managed_objects='/tip/v1/ManagedObjects/netact/cm/open-api/persistency/v1/managedObjects/'
-export api_url_configure_du='/tip/v1/ConfigureDU/oss/netconf'
-export api_url_configuration_management='/tip/v1/ConfigurationManagement/api/rest/configurationManagement/v1/bulkCM/importJobs/'
-export ne_id='DU_1499551/'
+export api_port=32080
+export api_url_container_health='/appmgr/ric/v1/health/ready'
